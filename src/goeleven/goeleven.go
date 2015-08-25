da37940617be0d88124ce90c50b5913611980878
Mads Freek <freek@wayf.dk>
2015-08-25 13:23:40 +0000

    now fairly resilient using facebooks gracehttp

--- src/goeleven/goeleven.go
+++ src/goeleven/goeleven.go
@@ -7,20 +7,21 @@ import (
 	"fmt"
 	"github.com/wayf-dk/pkcs11"
 	"io/ioutil"
+	"log"
+	//	"log/syslog"
+	"github.com/facebookgo/grace/gracehttp"
 	"net/http"
 	"os"
 	"regexp"
 	"strconv"
 	"strings"
 	"sync"
+	"syscall"
 	"time"
 )
 
 type Hsm struct {
 	session pkcs11.SessionHandle
-	used    int
-	started time.Time
-	sessno  int
 }
 
 type aclmap struct {
@@ -29,53 +30,99 @@ type aclmap struct {
 	label        string
 }
 
-var currentsessions int
-var hsm1 Hsm
-var sem chan Hsm
-var pguard sync.Mutex
-var p *pkcs11.Ctx
-var config = map[string]string{
-	"GOELEVEN_HSMLIB":        "",
-	"GOELEVEN_INTERFACE":     "localhost:8080",
-	"GOELEVEN_ALLOWEDIP":     "127.0.0.1",
-	"GOELEVEN_SLOT":          "",
-	"GOELEVEN_SLOT_PASSWORD": "",
-	"GOELEVEN_KEY_LABEL":     "",
-	"GOELEVEN_MINSESSIONS":   "1",
-	"GOELEVEN_MAXSESSIONS":   "1",
-	"GOELEVEN_MAXSESSIONAGE": "1000000",
-	"GOELEVEN_MECH":          "CKM_RSA_PKCS",
-	"GOELEVEN_DEBUG":         "false",
-	"SOFTHSM_CONF":           "softhsm.conf",
-	"GOELEVEN_HTTPS_KEY":     "false",
-	"GOELEVEN_HTTPS_CERT":    "false",
-}
+var (
+	contextmutex sync.RWMutex
+	context      = make(map[*http.Request]map[string]string)
 
-var keymap map[string]aclmap
+	maxsessions   int
+	p             *pkcs11.Ctx
+	sem           chan Hsm
+	pkcs11liblock chan int
 
-var sharedsecretlen = map[string]int{
-	"min": 12,
-	"max": 32,
-}
+	config = map[string]string{
+		"GOELEVEN_HSMLIB":        "",
+		"GOELEVEN_INTERFACE":     "localhost:8080",
+		"GOELEVEN_ALLOWEDIP":     "127.0.0.1",
+		"GOELEVEN_SLOT":          "",
+		"GOELEVEN_SLOT_PASSWORD": "",
+		"GOELEVEN_KEY_LABEL":     "",
+		"GOELEVEN_MAXSESSIONS":   "1",
+		"GOELEVEN_MECH":          "CKM_RSA_PKCS",
+		"SOFTHSM_CONF":           "softhsm.conf",
+		"GOELEVEN_HTTPS_KEY":     "false",
+		"GOELEVEN_HTTPS_CERT":    "false",
+	}
+
+	keymap map[string]aclmap
+
+	sharedsecretlen = map[string]int{
+		"min": 12,
+		"max": 32,
+	}
+)
 
 func main() {
-	currentsessions = 0
+	//	logwriter, e := syslog.New(syslog.LOG_NOTICE, "goeleven")
+	//	if e == nil {
+	//		log.SetOutput(logwriter)
+	//	}
+
 	keymap = make(map[string]aclmap)
-	//wd, _ := os.Getwd()
 	initConfig()
+
+	pkcs11liblock = make(chan int, 1)
+	pkcs11liblock <- 1
 	p = pkcs11.New(config["GOELEVEN_HSMLIB"])
-	p.Initialize()
-	handlesessions()
+
+	go bginit()
+
 	http.HandleFunc("/", handler)
 	var err error
 	if config["GOELEVEN_HTTPS_CERT"] == "false" {
-		err = http.ListenAndServe(config["GOELEVEN_INTERFACE"], nil)
+		gracehttp.Serve(
+			&http.Server{Addr: config["GOELEVEN_INTERFACE"], Handler: Log(http.DefaultServeMux)})
 	} else {
 		err = http.ListenAndServeTLS(config["GOELEVEN_INTERFACE"], config["GOELEVEN_HTTPS_CERT"], config["GOELEVEN_HTTPS_KEY"], nil)
 	}
 	if err != nil {
-		fmt.Printf("main(): %s\n", err)
+		log.Printf("main(): %s\n", err)
 	}
+	log.Printf("after ListenAndServer\n")
+}
+
+func bginit() {
+	// Init channel
+tryagain:
+	for {
+		if err := prepareobjects(config["GOELEVEN_KEY_LABEL"]); err != nil {
+			log.Printf("Waiting for HSM\n")
+			time.Sleep(5 * time.Second)
+			continue tryagain
+		}
+		break
+	}
+	go initpkcs11lib(false)
+}
+
+func Log(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		contextmutex.Lock()
+		ctx := make(map[string]string)
+		context[r] = ctx
+		contextmutex.Unlock()
+		starttime := time.Now()
+		handler.ServeHTTP(w, r)
+
+		status := 200
+		if ctx["err"] != "" {
+			status = 500
+		}
+		log.Printf("%s %s %s %1.3f %d/%d %d %s", r.RemoteAddr, r.Method, r.URL, time.Since(starttime).Seconds(), len(sem), cap(sem), status, ctx["err"])
+
+		contextmutex.Lock()
+		delete(context, r)
+		contextmutex.Unlock()
+	})
 }
 
 // initConfig read several Environment variables and based on them initialise the configuration
@@ -89,42 +136,43 @@ func initConfig() {
 		}
 	}
 	// All variable MUST have a value but we can not verify the variable content
-	for k, _ := range config {
-		if isdebug() {
-			// Don't write PASSWORD to debug
-			if k == "GOELEVEN_SLOT_PASSWORD" {
-				debug(fmt.Sprintf("%v: xxxxxx\n", k))
-			} else {
-				debug(fmt.Sprintf("%v: %v\n", k, config[k]))
-			}
+	for k, v := range config {
+		// Don't write PASSWORD to debug
+		if k == "GOELEVEN_SLOT_PASSWORD" {
+			v = "xxx"
 		}
-		if config[k] == "" {
-			exit(fmt.Sprintf("Problem with %s", k), 2)
-		}
+		log.Printf("%v: %v\n", k, v)
 	}
 
 	// Check file exists
 	for _, v := range envFiles {
 		_, err := os.Stat(config[v])
 		if err != nil {
-			exit(fmt.Sprintf("%s %s", v, err.Error()), 2)
+			log.Panicf("%s %s", v, err.Error())
 		}
 	}
+	max, _ := strconv.ParseInt(config["GOELEVEN_MAXSESSIONS"], 10, 0)
+	maxsessions = int(max)
 }
 
-func handlesessions() {
+// Prepareobjects returns a map of the label to object id for the given labels.
+// Returns an error if something goes wrong - the caller is supposed to keep trying
+// until no error is returned
+func prepareobjects(labels string) (err error) {
 	// String->int64->int convert
-	max, _ := strconv.ParseInt(config["GOELEVEN_MAXSESSIONS"], 10, 0)
-	var maxsessions int = int(max)
-	sem = make(chan Hsm, maxsessions)
-	for currentsessions < maxsessions {
-		currentsessions++
-		sem <- inithsm(currentsessions)
+	var s Hsm
+	err = p.Initialize()
+	defer p.Finalize()
+	s, err = initsession()
+	if err != nil {
+		return
 	}
+	defer p.CloseSession(s.session)
+	defer p.Logout(s.session)
 
-	s := <-sem
+	log.Printf("initsession result: %v\n", err)
 
-	keys := strings.Split(config["GOELEVEN_KEY_LABEL"], ",")
+	keys := strings.Split(labels, ",")
 
 	for _, v := range keys {
 
@@ -133,34 +181,33 @@ func handlesessions() {
 		sharedsecret := parts[1]
 		// Test validity of key specific sharedsecret
 		if len(sharedsecret) < sharedsecretlen["min"] || len(sharedsecret) > sharedsecretlen["max"] {
-			exit(fmt.Sprintf("problem with sharedsecret: '%s' for label: '%s'", sharedsecret, label), 2)
+			log.Panicf("problem with sharedsecret: '%s' for label: '%s'", sharedsecret, label)
 		}
 
 		template := []*pkcs11.Attribute{pkcs11.NewAttribute(pkcs11.CKA_LABEL, label), pkcs11.NewAttribute(pkcs11.CKA_CLASS, pkcs11.CKO_PRIVATE_KEY)}
-		if e := p.FindObjectsInit(s.session, template); e != nil {
-			panic(fmt.Sprintf("Failed to init: %s\n", e.Error()))
+		if err = p.FindObjectsInit(s.session, template); err != nil {
+			//syscall.Kill(syscall.Getpid(), syscall.SIGUSR2)
+			//panic(fmt.Sprintf("%d Failed to init: %s\n", syscall.Getpid(), e.Error()))
+			return errors.New("")
 		}
 		obj, b, e := p.FindObjects(s.session, 2)
 
-		debug(fmt.Sprintf("Obj %v\n", obj))
+		log.Printf("Obj %v\n", obj)
 		if e != nil {
-			exit(fmt.Sprintf("Failed to find: %s %v\n", e.Error(), b), 2)
+			log.Fatalf("Failed to find: %s %v\n", e.Error(), b)
 		}
 		if e := p.FindObjectsFinal(s.session); e != nil {
-			exit(fmt.Sprintf("Failed to finalize: %s\n", e.Error()), 2)
+			log.Fatalf("Failed to finalize: %s\n", e.Error())
 		}
-		debug(fmt.Sprintf("found keys: %v\n", len(obj)))
+		log.Printf("found keys: %v\n", len(obj))
 		if len(obj) == 0 {
-			exit(fmt.Sprintf("did not find a key with label '%s'", label), 2)
+			log.Fatalf("did not find a key with label '%s'", label)
 		}
 		keymap[label] = aclmap{obj[0], sharedsecret, label}
 	}
 
-	fmt.Printf("hsm initialized")
-
-	sem <- s
-
-	debug(fmt.Sprintf("sem: %v\n", len(sem)))
+	log.Printf("hsm initialized new: %#v\n", keymap)
+	return
 }
 
 // Client authenticate/authorization
@@ -187,6 +234,12 @@ func authClient(sharedkey string, slot string, keylabel string, mech string) err
 	return nil
 }
 
+// Utility to get correct error in log, while just sending generic 500 to client
+func logandsenderror(w http.ResponseWriter, err string, context map[string]string) {
+	context["err"] = err
+	http.Error(w, "Invalid Input", 500)
+}
+
 // TODO: Cleanup
 // TODO: Documentation
 // TODO: Error handling
@@ -196,6 +249,11 @@ func authClient(sharedkey string, slot string, keylabel string, mech string) err
  */
 func handler(w http.ResponseWriter, r *http.Request) {
 
+	defer r.Body.Close()
+	contextmutex.RLock()
+	ctx := context[r]
+	contextmutex.RUnlock()
+
 	ips := strings.Split(config["GOELEVEN_ALLOWEDIP"], ",")
 	ip := strings.Split(r.RemoteAddr, ":")
 	var allowed bool
@@ -204,47 +262,56 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if !allowed {
-		fmt.Println("unauthorised access attempt from:", r.RemoteAddr)
-		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		logandsenderror(w, "Unauthorised access attempt", ctx)
 		return
 	}
 
+	// handle non ok urls gracefully
 	var err error
 	var validPath = regexp.MustCompile("^/(\\d+)/([a-zA-Z0-9\\.]+)/sign$")
-	mSlot := validPath.FindStringSubmatch(r.URL.Path)[1]
-	mKeyAlias := validPath.FindStringSubmatch(r.URL.Path)[2]
+	match := validPath.FindStringSubmatch(r.URL.Path)
+	if match == nil {
+		logandsenderror(w, "Invalid path", ctx)
+		return
+	}
+	mSlot := match[1]
+	mKeyAlias := match[2]
 
-	defer r.Body.Close()
 	body, _ := ioutil.ReadAll(r.Body)
 
 	// Parse JSON
-	//var b struct { Data,Mech string }
-	var b map[string]interface{}
+	var b struct {
+		Data      string `json:"data"`
+		Mech      string `json:"mech"`
+		Sharedkey string `json:"sharedkey"`
+	}
 	err = json.Unmarshal(body, &b)
 	if err != nil {
-		http.Error(w, "Invalid input", 500)
-		fmt.Printf("json.unmarshall: %v\n", err.Error())
+		logandsenderror(w, fmt.Sprintf("json.unmarshall: %v", err.Error()), ctx)
 		return
 	}
-	data, err := base64.StdEncoding.DecodeString(b["data"].(string))
+
+	data, err := base64.StdEncoding.DecodeString(b.Data)
 	if err != nil {
-		http.Error(w, "Invalid input", 500)
-		fmt.Printf("DecodeString: %v\n", err.Error())
+		logandsenderror(w, fmt.Sprintf("DecodeString: %v", err.Error()), ctx)
+		return
+	}
+
+	if len(data) == 0 {
+		logandsenderror(w, "empty payload", ctx)
 		return
 	}
 
 	// Client auth
-	err = authClient(b["sharedkey"].(string), mSlot, mKeyAlias, b["mech"].(string))
+	err = authClient(b.Sharedkey, mSlot, mKeyAlias, b.Mech)
 	if err != nil {
-		http.Error(w, "Invalid input", 500)
-		fmt.Printf("authClient: %v\n", err.Error())
+		logandsenderror(w, fmt.Sprintf("authClient: %v", err.Error()), ctx)
 		return
 	}
 
-	sig, err, sessno := signing(data, mKeyAlias, r.RemoteAddr)
+	sig, err := signing(data, mKeyAlias)
 	if err != nil {
-		http.Error(w, "Invalid output", 500)
-		fmt.Printf("signing: %v %v\n", err.Error(), sessno)
+		logandsenderror(w, fmt.Sprintf("signing error: %s", err.Error()), ctx)
 		return
 	}
 	sigs := base64.StdEncoding.EncodeToString(sig)
@@ -256,87 +323,85 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	res := Res{mSlot, "mech", sigs}
 	json, err := json.Marshal(res)
 	if err != nil {
-		http.Error(w, "Invalid output", 500)
-		fmt.Printf("json.marshall: %v\n", err.Error())
+		logandsenderror(w, fmt.Sprintf("json.marshall: %s", err.Error()), ctx)
 		return
 	}
+
 	fmt.Fprintf(w, "%s\n\n", json)
+	r.Body.Close()
 }
 
-// TODO: Cleanup
-// TODO: Documentation
-func inithsm(sessno int) Hsm {
-	pguard.Lock()
-	defer pguard.Unlock()
-	slot, _ := strconv.ParseUint(config["GOELEVEN_SLOT"], 10, 32)
+func initpkcs11lib(restart bool) {
+	select {
+	case _ = <-pkcs11liblock:
+		log.Printf("initpkcs11lib restart %v\n", restart)
+		// send a SIGUSR2 signal - once - to self - to let grace start a new process for serving new requests
+		// we just fails ungracefully when a new process tells us to get lost ...
+		if restart {
+			syscall.Kill(syscall.Getpid(), syscall.SIGUSR2)
+			return
+		}
+	default:
+		return
+	}
 
-	fmt.Printf("slot: %v\n", slot)
-	session, e := p.OpenSession(uint(slot), pkcs11.CKF_SERIAL_SESSION)
+	sem = make(chan Hsm, maxsessions)
 
-	if e != nil {
-		// Try to reconnect
-		fmt.Printf("OpenSession error %s. Reinitialize hsm connection.", e.Error())
-		p.Initialize()
-		session, e = p.OpenSession(uint(slot), pkcs11.CKF_SERIAL_SESSION)
-		if e != nil {
-			//	panic(fmt.Sprintf("Failed to open session: %s\n", e.Error()))
-			exit(fmt.Sprintf("Terminate goeleven, failed to reopen session: %s\n", e.Error()), 2)
+	err := p.Initialize()
+	for err != nil {
+		if err.Error() == "pkcs11: 0x191: CKR_CRYPTOKI_ALREADY_INITIALIZED" {
+			err = nil
+		} else {
+			log.Printf("waiting: %v\n", err.Error())
+			time.Sleep(5 * time.Second)
+			err = p.Initialize()
 		}
 	}
 
-	p.Login(session, pkcs11.CKU_USER, config["GOELEVEN_SLOT_PASSWORD"])
+	for currentsessions := 0; currentsessions < maxsessions; currentsessions++ {
+		s, _ := initsession()
+		sem <- s
+	}
 
-	return Hsm{session, 0, time.Now(), sessno}
+	pkcs11liblock <- 1
+	log.Printf("initialized lib\n")
 }
 
 // TODO: Cleanup
 // TODO: Documentation
-func signing(data []byte, key string, remoteip string) ([]byte, error, int) {
-	// Pop HSM struct from queue
-	s := <-sem
-	s.used++
-	if s.used > 10000 || time.Now().Sub(s.started) > 1000*time.Second {
-		p.Logout(s.session)
-		p.CloseSession(s.session)
-		//p.Finalize()
-		//p.Destroy()
-		s = inithsm(s.sessno)
+func initsession() (Hsm, error) {
+	slot, _ := strconv.ParseUint(config["GOELEVEN_SLOT"], 10, 32)
+
+	log.Printf("slot: %v\n", slot)
+	session, e := p.OpenSession(uint(slot), pkcs11.CKF_SERIAL_SESSION)
+
+	if e != nil {
+		log.Printf("Failed to open session: %s\n", e.Error())
+		// panic(log.Sprintf("Failed to open session: %s\n", e.Error()))
 	}
-	fmt.Printf("label: %s handle: %d ip: %s\n", keymap[key].label, keymap[key].handle, remoteip)
+
+	e = p.Login(session, pkcs11.CKU_USER, config["GOELEVEN_SLOT_PASSWORD"])
+
+	if e != nil {
+		log.Printf("Failed to login to session: %s\n", e.Error())
+		// panic(log.Sprintf("Failed to open session: %s\n", e.Error()))
+	}
+
+	return Hsm{session}, e
+}
+
+// TODO: Cleanup
+// TODO: Documentation
+func signing(data []byte, key string) ([]byte, error) {
+	var err error
+	s := <-sem
+
 	p.SignInit(s.session, []*pkcs11.Mechanism{pkcs11.NewMechanism(pkcs11.CKM_RSA_PKCS, nil)}, keymap[key].handle)
 	sig, err := p.Sign(s.session, data)
+	if err != nil {
+		go initpkcs11lib(true)
+	}
 
-	// Push HSM struct back on queue
 	sem <- s
-	return sig, err, s.sessno
-}
-
-// Utils
-
-func debug(messages string) {
-	if config["GOELEVEN_DEBUG"] == "true" {
-		fmt.Print(messages)
-	}
-}
-
-// Standard function to test for debug mode
-func isdebug() bool {
-	if config["GOELEVEN_DEBUG"] == "true" {
-		return true
-	} else {
-		return false
-	}
-}
-
-func exit(messages string, errorCode int) {
-	// Exit code and messages based on Nagios plugin return codes (https://nagios-plugins.org/doc/guidelines.html#AEN78)
-	var prefix = map[int]string{0: "OK", 1: "Warning", 2: "Critical", 3: "Unknown"}
-
-	// Catch all unknown errorCode and convert them to Unknown
-	if errorCode < 0 || errorCode > 3 {
-		errorCode = 3
-	}
-
-	fmt.Printf("%s %s\n", prefix[errorCode], messages)
-	os.Exit(errorCode)
+	return sig, err
 }

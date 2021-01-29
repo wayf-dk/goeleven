4e2c49753ec64fbb3c3f6dfcb8b0b659604ca91d
Mads Freek <freek@wayf.dk>
2021-01-29 17:31:46 +0100

    Refactored project structure

---
+++ cmd/goeleven/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/wayf-dk/goeleven"
+)
+
+func main() {
+	goeleven.Main()
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/wayf-dk/goeleven
 
 go 1.15
+
+require github.com/miekg/pkcs11 v1.0.3
